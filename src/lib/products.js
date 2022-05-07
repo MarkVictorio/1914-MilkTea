dad3e02f56cf9016f08d0fb7b57cd1af77099e37
John Noah Jarry <johnnoahjarry16@gmail.com>
2022-05-08 00:28:36 +0800

    add prices to products in products.js file

--- src/lib/products.js
+++ src/lib/products.js
@@ -26,116 +26,139 @@ export default [
     {
         image: ferreroMilkTea,
         name: "Ferrero Milk Tea",
-        description: "Indulge in 1914 Milk Tea’s Select Imported Chocolates. Enjoy the taste of Ferrero in every sip with our special Ferrero Milk Tea."
+        description: "Indulge in 1914 Milk Tea’s Select Imported Chocolates. Enjoy the taste of Ferrero in every sip with our special Ferrero Milk Tea.",
+        price: "P165.00"
     },
     {
         image: mAndMMilkTea,
         name: "M&M Milk Tea",
-        description: "Indulge in 1914 Milk Tea’s Select Imported Chocolates. Bring the party in a cup with 1914’s M&M Milk tea, melts in your mouth not on your hands, get the M&M experience in a new and exciting way!"
+        description: "Indulge in 1914 Milk Tea’s Select Imported Chocolates. Bring the party in a cup with 1914’s M&M Milk tea, melts in your mouth not on your hands, get the M&M experience in a new and exciting way!",
+        price: "P165.00"
     },
     {
         image: tobleroneChocolate,
         name: "Toblerone Chocolate",
-        description: "Indulge in 1914 Milk Tea’s Select Imported Chocolates. The classic Toblerone is here, beat the heat and enjoy the classic chocolate flavor of Toblerone now in a Milk-Tea way!"
+        description: "Indulge in 1914 Milk Tea’s Select Imported Chocolates. The classic Toblerone is here, beat the heat and enjoy the classic chocolate flavor of Toblerone now in a Milk-Tea way!",
+        price: "P165.00"
     },
     {
         image: oreoCheesecake,
         name: "Oreo Cheesecake",
-        description: "Looking for something more adventurous? The Cheesecake Series by 1914 Milk Tea Bar will satisfy you. Enjoy an Oreo Cheesecake on the go, each sip will give you that twist-lick-dunk experience you’ve been waiting for!"
+        description: "Looking for something more adventurous? The Cheesecake Series by 1914 Milk Tea Bar will satisfy you. Enjoy an Oreo Cheesecake on the go, each sip will give you that twist-lick-dunk experience you’ve been waiting for!",
+        price: "P165.00"
     },
     {
         image: blueberryCheesecakeMilkTea,
         name: "Blueberry Cheesecake Milk Tea",
-        description: "Looking for something more adventurous?  The Cheesecake Series by 1914 Milk Tea Bar will satisfy you. Blueberry Cheesecake is here for you to enjoy! Get a burst of Blueberry and the taste of cheesecake with every sip!"
+        description: "Looking for something more adventurous?  The Cheesecake Series by 1914 Milk Tea Bar will satisfy you. Blueberry Cheesecake is here for you to enjoy! Get a burst of Blueberry and the taste of cheesecake with every sip!",
+        price: "P165.00"
     },
     {
         image: matchaCheesecake,
         name: "Matcha Cheesecake",
-        description: "Looking for something more adventurous?  The Cheesecake Series by 1914 Milk Tea Bar will satisfy you. Get that green tea experience along with a pop of cheesecake in a cup!"
+        description: "Looking for something more adventurous?  The Cheesecake Series by 1914 Milk Tea Bar will satisfy you. Get that green tea experience along with a pop of cheesecake in a cup!",
+        price: "P165.00"
     },
     {
         image: pomegranateOrganic,
         name: "Pomegranate Organic",
-        description: "Feeling healthy? Get a cup of organic Pomegranate with real fruit bits with this special drink!"
+        description: "Feeling healthy? Get a cup of organic Pomegranate with real fruit bits with this special drink!",
+        price: "P165.00"
     },
     {
         image: basicMilkTea,
-        name: "Basic Milk Tea P145",
-        description: "You can never go wrong with the basic! Take a break from all the exotic flavors and enjoy the classic, tried and true Basic Milk Tea by 1914 Milk Tea Bar."
+        name: "Basic Milk Tea",
+        description: "You can never go wrong with the basic! Take a break from all the exotic flavors and enjoy the classic, tried and true Basic Milk Tea by 1914 Milk Tea Bar.",
+        price: "P145.00"
     },
     {
         image: strawberryMilkTea,
-        name: "Strawberry Milk Tea P150",
-        description: "You can never go wrong with the basic! The classic Strawberry you know and love is here in a cup."
+        name: "Strawberry Milk Tea",
+        description: "You can never go wrong with the basic! The classic Strawberry you know and love is here in a cup.",
+        price: "P150.00"
     },
     {
         image: cookiesAndCreamMilkTea,
-        name: "Cookies & Cream Milk Tea P150",
-        description: "You can never go wrong with the basic! Cookies? Cream? The classic combination all in one fantastic cup!"
+        name: "Cookies & Cream Milk Tea",
+        description: "You can never go wrong with the basic! Cookies? Cream? The classic combination all in one fantastic cup!",
+        price: "P150.00"
     },
     {
         image: blueberryMilkTea,
         name: "Blueberry Milk Tea",
-        description: "Treat yourself with something a little more premium. Blueberry Milk Tea with a richer and fuller taste!"
+        description: "Treat yourself with something a little more premium. Blueberry Milk Tea with a richer and fuller taste!",
+        price: "P150.00"
     },
     {
         image: cheesecakePremiumMilkTea,
         name: "Cheesecake Premium Milk Tea",
-        description: "Treat yourself with something a little more premium. Cheesecake, Milk Tea tried and true served in a premium cup just for you!"
+        description: "Treat yourself with something a little more premium. Cheesecake, Milk Tea tried and true served in a premium cup just for you!",
+        price: "P150.00"
     },
     {
         image: javaChipsPremiumMilkTea,
         name: "Java Chips Premium Milk Tea",
-        description: "Treat yourself with something a little more premium. Enjoy Java Chips in a cool, premium cup!"
+        description: "Treat yourself with something a little more premium. Enjoy Java Chips in a cool, premium cup!",
+        price: "P150.00"
     },
     {
         image: okinawaPremiumMilkTea,
         name: "Okinawa Premium Milk Tea",
-        description: "Treat yourself with something a little more premium. Get a breath of Okinawa with our premium selection."
+        description: "Treat yourself with something a little more premium. Get a breath of Okinawa with our premium selection.",
+        price: "P150.00"
     },
     {
         image: redVelvetPremiumMilkTea,
         name: "Red Velvet Premium Milk Tea",
-        description: "Treat yourself with something a little more premium. Roll the red carpet with our Red Velvet Premium Milk Tea, feel the rich flavor in every sip."
+        description: "Treat yourself with something a little more premium. Roll the red carpet with our Red Velvet Premium Milk Tea, feel the rich flavor in every sip.",
+        price: "P150.00"
     },
     {
         image: darkChocolatePremiumMilkTea,
         name: "Dark Chocolate Premium Milk Tea",
-        description: "Treat yourself with something a little more premium. Indulge in a cup of Premium Dark Chocolate Milk Tea, the rich dark taste will treat your taste buds!"
+        description: "Treat yourself with something a little more premium. Indulge in a cup of Premium Dark Chocolate Milk Tea, the rich dark taste will treat your taste buds!",
+        price: "P150.00"
     },
     {
         image: saltedCaramelPremiumMilkTea,
         name: "Salted Caramel Premium Milk Tea",
-        description: "Treat yourself with something a little more premium. Nothing goes wrong with a little Salt and Caramel, this sweet and salty will surely make your day!"
+        description: "Treat yourself with something a little more premium. Nothing goes wrong with a little Salt and Caramel, this sweet and salty will surely make your day!",
+        price: "P150.00"
     },
     {
         image: taroPremiumMilkTea,
         name: "Taro Premium Milk Tea",
-        description: "Treat yourself with something a little more premium. Get some taro goodness in a cup!"
+        description: "Treat yourself with something a little more premium. Get some taro goodness in a cup!",
+        price: "P150.00"
     },
     {
         image: blueberryFruitTea,
         name: "Blueberry Fruit Tea",
-        description: "Up for blend? The Fruit Tea Collection will hit the spot! Blueberry Fruit tea will tangle your taste buds with the sweet blueberry taste."
+        description: "Up for blend? The Fruit Tea Collection will hit the spot! Blueberry Fruit tea will tangle your taste buds with the sweet blueberry taste.",
+        price: "P140.00"
     },
     {
         image: greenAppleFruitTea,
         name: "Green Apple Fruit Tea",
-        description: "Up for blend? The Fruit Tea Collection will hit the spot! There is never a bad apple, the Green Apple Tea will take you to an adventure!"
+        description: "Up for blend? The Fruit Tea Collection will hit the spot! There is never a bad apple, the Green Apple Tea will take you to an adventure!",
+        price: "P140.00"
     },
     {
         image: lycheeFruitTea,
         name: "Lychee Fruit Tea",
-        description: "Up for blend? The Fruit Tea Collection will hit the spot! Lay down and get some Lychee, the perfect pair for a sunny day or any day!"
+        description: "Up for blend? The Fruit Tea Collection will hit the spot! Lay down and get some Lychee, the perfect pair for a sunny day or any day!",
+        price: "P140.00"
     },
     {
         image: strawberryTonica,
         name: "Strawberry Tonica",
-        description: "Up your health game with the Tonica Series! Get a healthy sip of strawberry in every cup."
+        description: "Up your health game with the Tonica Series! Get a healthy sip of strawberry in every cup.",
+        price: "P130.00"
     },
     {
         image: blueberryTonica,
         name: "Blueberry Tonica",
-        description: "Up your health game with the Tonica Series! Get a healthy sip of blueberry in every cup."
+        description: "Up your health game with the Tonica Series! Get a healthy sip of blueberry in every cup.",
+        price: "P130.00"
     },
 ]
